53d4de7915720b1d65f9f5dccb841cd2ef032583
svsnarasimharao123 <svss.narasimharao123@gmail.com>
2020-03-27 11:15:27 +0530

    modified

--- reports/cucumber-html-report/report.js
+++ reports/cucumber-html-report/report.js
@@ -8,7 +8,7 @@ formatter.feature({
   "tags": [
     {
       "line": 1,
-      "name": "@Demoblaze_feature"
+      "name": "@Demoblazefeature"
     }
   ]
 });
@@ -55,35 +55,35 @@ formatter.match({
   "location": "Signup_stepdefinition.the_user_launches_the_browser()"
 });
 formatter.result({
-  "duration": 10747598100,
+  "duration": 11076356600,
   "status": "passed"
 });
 formatter.match({
   "location": "Signup_stepdefinition.the_user_opens_the_application()"
 });
 formatter.result({
-  "duration": 1731733600,
+  "duration": 1320803400,
   "status": "passed"
 });
 formatter.match({
   "location": "Signup_stepdefinition.the_user_clicks_the_sign_up_link()"
 });
 formatter.result({
-  "duration": 1227792300,
+  "duration": 1131081900,
   "status": "passed"
 });
 formatter.match({
   "location": "Signup_stepdefinition.the_user_enter_the_username_and_password_and_clicks_on_singup()"
 });
 formatter.result({
-  "duration": 86206861600,
+  "duration": 84822340300,
   "status": "passed"
 });
 formatter.match({
   "location": "Signup_stepdefinition.take_a_Screenshot_of_Sign_up()"
 });
 formatter.result({
-  "duration": 2693028400,
+  "duration": 2590084300,
   "status": "passed"
 });
 formatter.scenarioOutline({
@@ -159,7 +159,7 @@ formatter.scenario({
     },
     {
       "line": 1,
-      "name": "@Demoblaze_feature"
+      "name": "@Demoblazefeature"
     }
   ]
 });
@@ -191,14 +191,14 @@ formatter.match({
   "location": "Login_stepdefinition.the_user_launch_the_application()"
 });
 formatter.result({
-  "duration": 11904363100,
+  "duration": 11087504400,
   "status": "passed"
 });
 formatter.match({
   "location": "Login_stepdefinition.the_user_clicks_on_login_link()"
 });
 formatter.result({
-  "duration": 149710900,
+  "duration": 162828800,
   "status": "passed"
 });
 formatter.match({
@@ -215,16 +215,15 @@ formatter.match({
   "location": "Login_stepdefinition.the_user_enter_username_as_password(String,String)"
 });
 formatter.result({
-  "duration": 2500636200,
+  "duration": 2758101800,
   "status": "passed"
 });
 formatter.match({
   "location": "Login_stepdefinition.click_on_login_button()"
 });
 formatter.result({
-  "duration": 5094164000,
-  "error_message": "org.openqa.selenium.UnhandledAlertException: unexpected alert open: {Alert text : Wrong password.}\n  (Session info: chrome\u003d80.0.3987.149): Wrong password.\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-HPDV7GE\u0027, ip: \u0027192.168.0.105\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002714\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\opava\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:56570}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 0255c1fb6fa00f89f2e198fb824455d2\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:120)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:295)\r\n\tat com.pages.Login_page.login_buttonclick(Login_page.java:75)\r\n\tat com.stepdefinition.Login_stepdefinition.click_on_login_button(Login_stepdefinition.java:41)\r\n\tat ✽.Then click on login button(src/main/resources/Feature/demoblazetest.feature:17)\r\n",
-  "status": "failed"
+  "duration": 8683378800,
+  "status": "passed"
 });
 formatter.scenario({
   "line": 25,
@@ -264,28 +263,28 @@ formatter.match({
   "location": "MonitorsSelect_stepdefinition.the_user_launch_the_demoblaze_application()"
 });
 formatter.result({
-  "duration": 11627690700,
+  "duration": 19048149600,
   "status": "passed"
 });
 formatter.match({
   "location": "MonitorsSelect_stepdefinition.the_user_clicks_on_montiors_under_categories()"
 });
 formatter.result({
-  "duration": 142950200,
+  "duration": 1344793900,
   "status": "passed"
 });
 formatter.match({
   "location": "MonitorsSelect_stepdefinition.the_user_click_on_the_reuired_monitor_and_added_to_cart()"
 });
 formatter.result({
-  "duration": 1541157200,
+  "duration": 1855995700,
   "status": "passed"
 });
 formatter.match({
   "location": "MonitorsSelect_stepdefinition.the_user_get_a_message()"
 });
 formatter.result({
-  "duration": 6864193200,
+  "duration": 3500586000,
   "status": "passed"
 });
 formatter.scenario({
@@ -326,28 +325,28 @@ formatter.match({
   "location": "Contact_stepdefinition.the_user_launch_the_demoblaze_site()"
 });
 formatter.result({
-  "duration": 13387119900,
+  "duration": 12303207000,
   "status": "passed"
 });
 formatter.match({
   "location": "Contact_stepdefinition.the_user_clicks_on_contact_link()"
 });
 formatter.result({
-  "duration": 110085500,
+  "duration": 178618900,
   "status": "passed"
 });
 formatter.match({
   "location": "Contact_stepdefinition.the_user_enter_the_email_name_and_message()"
 });
 formatter.result({
-  "duration": 638402300,
+  "duration": 989001100,
   "status": "passed"
 });
 formatter.match({
   "location": "Contact_stepdefinition.the_user_get_a_pop_window_message()"
 });
 formatter.result({
-  "duration": 3872069600,
+  "duration": 3834494300,
   "status": "passed"
 });
 formatter.scenario({
@@ -383,21 +382,21 @@ formatter.match({
   "location": "Description_stepdefinition.the_user_launches_the_demoblaze_website()"
 });
 formatter.result({
-  "duration": 14798358100,
+  "duration": 10874534600,
   "status": "passed"
 });
 formatter.match({
   "location": "Description_stepdefinition.the_user_scrolldown_the_website_page()"
 });
 formatter.result({
-  "duration": 4508875400,
+  "duration": 4584261600,
   "status": "passed"
 });
 formatter.match({
   "location": "Description_stepdefinition.the_user_check_the_information_available_and_get_the_description_on_console()"
 });
 formatter.result({
-  "duration": 6164631200,
+  "duration": 2300994400,
   "status": "passed"
 });
 });
